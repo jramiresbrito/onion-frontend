7fb8db14b57f5e529c19c7f4aa7af6726bd37844
Joao Victor Ramires <joaoramires.engsoft@gmail.com>
2022-05-07 20:50:38 -0300

    Add React Modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,37 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
 import { Logo } from '../Logo/index';
+
 import { Container, Content } from './styles';
 
+Modal.setAppElement('#root');
+
 export function Header() {
+  const [satelitesModalIsOpened, setSatelitesModalIsOpened] = useState(false);
+
+  function handleOpenSatelitesModal() {
+    setSatelitesModalIsOpened(true);
+  }
+
+  function handleCloseSatelitesModal() {
+    setSatelitesModalIsOpened(false);
+  }
+
   return (
     <Container>
       <Content>
         <Logo />
 
-        <button type="button">Find Satelites</button>
+        <button type="button" onClick={handleOpenSatelitesModal}>
+          Find Satelites
+        </button>
+
+        <Modal
+          isOpen={satelitesModalIsOpened}
+          onRequestClose={handleCloseSatelitesModal}
+        >
+          <h2>Find Nearby Satelites</h2>
+        </Modal>
       </Content>
     </Container>
   );
